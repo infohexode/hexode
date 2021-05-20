4773a233900aa2a6bdbe9295341bf197ae2b7a03
infohexode <80357152+infohexode@users.noreply.github.com>
2021-05-21 01:35:20 +0530

    initial commit

---
+++ src/app/article-lists/article-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-lists',
+  templateUrl: './article-lists.component.html',
+  styleUrls: ['./article-lists.component.css']
+})
+export class ArticleListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
